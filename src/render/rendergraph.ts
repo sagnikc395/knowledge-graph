451cdb190b862fede14af7fb847a6a41d26f1ccb
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-06-01 18:27:00 +0530

    feat: added draw edges

--- src/render/rendergraph.ts
+++ src/render/rendergraph.ts
@@ -1,5 +1,27 @@
 import type { Edge, Node } from "../graph/types";
 
+//draw the edges of the graph 
+
+function drawEdges(nodes: Array<Node>, edges: Array<Edge>, ctx: CanvasRenderingContext2D) {
+    for (const edge of edges) {
+        const fromNode = nodes.find(n => n.id === edge.from);
+        const toNode = nodes.find(n => n.id === edge.to);
+
+        if (!fromNode || !toNode) {
+            throw new Error(`starting node or ending node not found!`)
+        }
+        ctx.beginPath();
+        ctx.moveTo(fromNode.x, fromNode.y);
+        ctx.lineTo(toNode.x, toNode.y);
+        ctx.strokeStyle = "#555";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
+
+}
+
+
+
 function drawGraph(canvasId: string, nodes: Array<Node>, edges: Array<Edge>): void {
     const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
     const ctx = canvas.getContext("2d");
